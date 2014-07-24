151beb19fd20bac3cc91bbbdf06f54311546d591
Augusto Dantas <augustold42@gmail.com>
2014-07-23 23:40:15 -0300

    TokenType e Tokenizer

--- src/Token.java
+++ src/Token.java
@@ -3,12 +3,15 @@ import java.io.*;
 public class Token {
   
 	public enum TokenType {
+		ATTR, //Attribution
 		INT_TYPE, CHAR_TYPE, BOOL_TYPE, 
 		IF_DECL, ELSE_DECL, 
 		WHILE_DECL,
-		NUMERIC, STRING,
+		NUMBER, CHAR, BOOL,
 		ID, 
-		SUM, DIF, MULT, DIV
+		SUM, DIF, MULT, DIV,
+		//Equal, LesserThan, GreaterThan, LesserOrEqual, GreaterOrEqual, NotEqual
+		EQ, LT, GT, LE, GE, NE
 	};
 
 	TokenType tipo;
--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -15,10 +15,10 @@ public class Tokenizer {
 		try {
 			st = new StreamTokenizer(br);
 			while (st.nextToken() != StreamTokenizer.TT_EOF) {
-				if (String.valueOf((char) st.ttype).equals("\"")) {
-					tokens.add(new Token(Token.TokenType.STRING, st.sval, 0.0));
+				if (String.valueOf((char) st.ttype).equals("\'")) {
+					tokens.add(new Token(Token.TokenType.CHAR, st.sval, 0.0));
 				} else if (st.ttype == StreamTokenizer.TT_NUMBER) {
-					tokens.add(new Token(Token.TokenType.NUMERIC, "", st.nval));
+					tokens.add(new Token(Token.TokenType.NUMBER, "", st.nval));
 				} else if(st.ttype == StreamTokenizer.TT_WORD) {
 					tokens.add(buildWordToken(st.sval));
 				} else if ((st.ttype != StreamTokenizer.TT_EOF) || (st.ttype != StreamTokenizer.TT_EOL)) {
@@ -38,6 +38,27 @@ public class Tokenizer {
 		case "IF": 
 			tt = Token.TokenType.IF_DECL; 
 			break;
+		case "ELSE": 
+			tt = Token.TokenType.ELSE_DECL; 
+			break;
+		case "WHILE": 
+			tt = Token.TokenType.WHILE_DECL; 
+			break;
+		case "int": 
+			tt = Token.TokenType.INT_TYPE; 
+			break;
+		case "char": 
+			tt = Token.TokenType.CHAR_TYPE; 
+			break;
+		case "bool": 
+			tt = Token.TokenType.BOOL_TYPE; 
+			break;
+		case "true": 
+			tt = Token.TokenType.BOOL; 
+			break;
+		case "false": 
+			tt = Token.TokenType.BOOL; 
+			break;
 		default:
 			tt = Token.TokenType.ID;
         	sval = word;
@@ -51,11 +72,38 @@ public class Tokenizer {
 		String simbolo = String.valueOf((char) val);
 
 		switch(simbolo) {
+		case "=": 
+				tt = Token.TokenType.ATTR; 
+				break;
 		case "+": 
 			tt = Token.TokenType.SUM; 
 			break;
-		default:
-			tt = Token.TokenType.ID;
+		case "-": 
+			tt = Token.TokenType.DIF; 
+			break;
+		case "*": 
+			tt = Token.TokenType.MULT; 
+			break;
+		case "/": 
+			tt = Token.TokenType.DIV; 
+			break;
+		case "==": 
+			tt = Token.TokenType.EQ; 
+			break;
+		case ">": 
+			tt = Token.TokenType.GT; 
+			break;
+		case "<": 
+			tt = Token.TokenType.LT; 
+			break;
+		case ">=": 
+			tt = Token.TokenType.GE; 
+			break;
+		case "<=": 
+			tt = Token.TokenType.LE; 
+			break;
+		case "!=": 
+			tt = Token.TokenType.NE; 
 			break;
 		}
 		return(new Token(tt, simbolo, 0.0));
