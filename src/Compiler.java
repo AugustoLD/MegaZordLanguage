39d7d9301373e4faa86b4493bf2fbc0c7319c5ed
Augusto Dantas <augustold42@gmail.com>
2014-10-24 19:26:14 -0200

    Missing Expression Validation

--- src/Compiler.java
+++ src/Compiler.java
@@ -6,6 +6,7 @@ public class Compiler {
 	public static void main(String args[]) {
 		BufferedReader br;
 		Tokenizer tok;
+		Synthesizer syn;
 		ArrayList<Token> tokenList = new ArrayList<>();
 		SymbolTable symbolTable = new SymbolTable();
 	  
@@ -13,9 +14,6 @@ public class Compiler {
 			br = new BufferedReader(new FileReader(args[0]));
 			tok = new Tokenizer(br);
 			tok.run(tokenList, symbolTable);
-			
-			
-			/* chama aqui */
 
 			if(args.length > 1 && args[1].equals("simple")){
 				System.out.println("==Token List==");
@@ -30,6 +28,8 @@ public class Compiler {
 			System.out.println("\n\n==Symbol Table (HashTable)==");
 			System.out.println(symbolTable.toString());
 			
+			syn = new Synthesizer(tokenList);
+			syn.run();
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
--- src/Synthesizer.java
+++ src/Synthesizer.java
@@ -1,49 +1,198 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class Synthesizer {
-	private int pos = 0;
+	private int iterator = 0;
 	List<Token> tokenList;
-	List<TokenType> braceStack = new ArrayList<TokenType>();
-	List<TokenType> parenthesisStack = new ArrayList<TokenType>();
+	int braceStack;
 	
-	public void run(List<Token> tokenList) {
+	public Synthesizer(List<Token> tokenList) {
 		this.tokenList = tokenList;
-		checkOutset();
+		this.iterator = 0;
+		this.braceStack = 0;
+	}
+	
+	public void run() throws Exception {
+		/* Checking program beginning
+		 * Example: program_name() {
+		 */
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.ID) {
+			// missing program name
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected program name and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		} 
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.L_PAR) {
+			// missing opening parenthesis
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ( and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.R_PAR) {
+			// missing closing parenthesis
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ) and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.L_BRACE) {
+			// missing opening brace
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected { and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		} else {
+			braceStack++;
+		}
+		
+		checkBody();
+		
+		if(braceStack != 0) {
+			// missing closing brace
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected } and got EOF instead");
+		}
+		if(iterator < tokenList.size()) {
+			// exceeding closing brace
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected EOF and got \"" + tokenList.get(iterator).getKey() + "\" instead");
+		}	
+	}
+	
+	private void checkBody() throws Exception {
 		Token token;
 		
-		while(pos < tokenList.size()) {
-			token = tokenList.get(pos++);
+		while(iterator < tokenList.size()) {
+			token = tokenList.get(iterator++);
 			if(token.getType() == TokenType.BOOL_TYPE || token.getType() == TokenType.CHAR_TYPE ||
 					token.getType() == TokenType.INT_TYPE) {
-				
+				checkVarDeclaration();
+			} else if(token.getType() == TokenType.IF_DECL) {
+				checkIfDeclaration();
+			} else if(token.getType() == TokenType.WHILE_DECL) {
+				checkWhileDeclaration();
+			} else if(token.getType() == TokenType.ID) {
+				checkAttribution();
+			} else if(token.getType() == TokenType.PRINT) {
+				checkPrintFunction();
+			} else if(token.getType() == TokenType.R_BRACE) {
+				braceStack--;
+				return;
 			}
 		}
 	}
 	
-	public void checkOutset() {
-		if(tokenList.get(pos++).getType() != TokenType.ID) {
-			// erro nome do programa
-		} 
-		if(tokenList.get(pos++).getType() != TokenType.L_PAR) {
-			// erro parentese não aberto
+	private void checkVarDeclaration() throws Exception {
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.ID) {
+			// missing identificator
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected identificator and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
 		}
-		if(tokenList.get(pos++).getType() != TokenType.R_PAR) {
-			// erro parentese não fechado
-		}
-		if(tokenList.get(pos++).getType() != TokenType.L_BRACE) {
-			// erro chave não aberta
-		} else {
-			braceStack.add(TokenType.L_BRACE);
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.SEMICOLLON) {
+			// missing semicollon
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ; and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
 		}
 	}
 	
-	public void checkVarDeclaration() {
-		if(tokenList.get(pos++).getType() != TokenType.ID) {
-			// erro declaração variável
+	private void checkIfDeclaration() throws Exception {
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.L_PAR) {
+			// missing opening parenthesis
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ( and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
 		}
-		if(tokenList.get(pos++).getType() != TokenType.SEMICOLLON) {
-			// erro ; expected
+		
+		//TODO
+		// checkLogicExpression();
+		
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.R_PAR) {
+			// missing closing parenthesis
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ) and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.L_BRACE) {
+			// missing opening brace
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected { and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		} else {
+			braceStack++;
+		}
+		
+		checkBody();
+		// checking if there is an else statement in case of a IF_DECL
+		if(iterator < tokenList.size() && tokenList.get(iterator).getType() == TokenType.ELSE_DECL) {
+			iterator++;
+			checkBody();
+		}
+	}
+	
+	private void checkWhileDeclaration() throws Exception {
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.L_PAR) {
+			// missing opening parenthesis
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ( and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+		
+		//TODO
+		// checkLogicExpression();
+		
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.R_PAR) {
+			// missing closing parenthesis
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ) and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.L_BRACE) {
+			// missing opening brace
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected { and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		} else {
+			braceStack++;
+		}
+		checkBody();
+	}
+	
+	private void checkAttribution() throws Exception {
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.ATTR) {
+			// missing attribution symbol
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected = and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+		
+		//TODO
+		// checkArithmeticExpression();
+		
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.SEMICOLLON) {
+			// missing semicollon
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ; and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+	}
+	
+	private void checkPrintFunction() throws Exception {
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.L_PAR) {
+			// missing opening parenthesis
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ( and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+		
+		checkMessage();
+		
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.R_PAR) {
+			// missing closing parenthesis
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ) and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.SEMICOLLON) {
+			// missing semicollon
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected ; and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+	}
+	
+	private void checkMessage() throws Exception {
+		if(iterator < tokenList.size() && tokenList.get(iterator++).getType() != TokenType.ID &&
+				tokenList.get(iterator-1).getType() != TokenType.STRING) {
+			// missing print arguments
+			throw new Exception("Systax Error at line " + tokenList.get(iterator-1).getLine() + 
+					": Expected print argument and got \"" + tokenList.get(iterator-1).getKey() + "\" instead");
+		}
+		
+		if(iterator < tokenList.size() && tokenList.get(iterator).getType() == TokenType.COMMA) {
+			iterator++;
+			checkMessage();
 		}
 	}
 }
